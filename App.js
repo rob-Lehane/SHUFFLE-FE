ee01b574d2cc03891153ea7600909e93651200bd
Louis <roachlouis@hotmail.com>
2023-11-06 14:31:40 +0000

    html code and button

--- App.js
+++ App.js
@@ -18,3 +18,40 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+document.getElementById('play-button').addEventListener('click', function() {
+  let audio = document.getElementById('music-player');
+  audio.play();
+});
+
+document.getElementById('pause-button').addEventListener('click', function() {
+  let audio = document.getElementById('music-player');
+  audio.pause();
+});
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <div class="music-widget">
+        <div class="song-info">
+            <img src="album-cover.jpg" alt="Album Cover">
+            <h2>Song Title</h2>
+            <p>Artist Name</p>
+        </div>
+        <div class="player-controls">
+            <button id="play-button">Play</button>
+            <button id="pause-button">Pause</button>
+        </div>
+    </div>
+
+    <audio id="music-player" controls>
+        <source src="song.mp3" type="audio/mpeg">
+        Your browser does not support the audio element.
+    </audio>
+    
+    <script src="script.js"></script>
+</body>
+</html>
