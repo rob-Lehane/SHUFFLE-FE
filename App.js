e4e142baf7644fb36d40766acc211cb5aebbde24
Joe Bailey <joebailey1000@hotmail.co.uk>
2023-11-06 15:15:31 +0000

    sound now playing

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
 export default function App() {
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View class="music-widget">
+        <View class="song-info">
+            <Image src="album-cover.jpg" alt="Album Cover"/>
+            <h2>Song Title</h2>
+            <p>Artist Name</p>
+        </View>
+        <View class="player-controls">
+            <Button id="play-button" title='Play' onPress={()=>{
+               let audio = document.getElementById('music-player');
+               audio.play();
+            }}/>
+            <Button id="pause-button" title='Pause' onPress={()=>{
+              let audio = document.getElementById('music-player');
+              audio.pause();
+            }}/>
+        </View>
+      </View>
+      <audio id='music-player' src='https://cdns-preview-6.dzcdn.net/stream/c-67cc9a21a99509f600d285378314110b-3.mp3'/>
     </View>
   );
 }
