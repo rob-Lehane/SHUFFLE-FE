3631215320590168d94c5f44abdae6954ea7b344
Louis <roachlouis@hotmail.com>
2023-11-13 16:20:23 +0000

    login working

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, Button, Pressable } from 'react-native';
 import { useEffect, useState } from 'react';
-import axios from 'axios'
-import { Rating } from 'react-native-ratings'
+import axios from 'axios';
+import { Rating } from 'react-native-ratings';
 import { History } from './History';
 import { AudioPlayer } from './AudioPlayer';
 import { TextInput } from 'react-native';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const MyTheme = {
   ...DefaultTheme,
@@ -16,49 +17,84 @@ const MyTheme = {
   },
 };
 
-
 export default function App() {
-  const [historyShowing, setHistoryShowing] = useState(false)
-  const [songHistory, setSongHistory] = useState([])
-  const [user, setUser] = useState()
-  const [loginInput, setLoginInput] = useState('')
-  const [loginError, setLoginError] = useState(false)
+  const [historyShowing, setHistoryShowing] = useState(false);
+  const [songHistory, setSongHistory] = useState([]);
+  const [user, setUser] = useState(null);
+  const [loginInput, setLoginInput] = useState('');
+  const [loginError, setLoginError] = useState(false);
 
+  useEffect(() => {
+   
+    checkUserLoggedIn();
+  }, []);
 
+  const checkUserLoggedIn = async () => {
+    try {
+      const userString = await AsyncStorage.getItem('user');
+      if (userString) {
+        const storedUser = JSON.parse(userString);
+        setUser(storedUser);
+      }
+    } catch (error) {
+      console.error('Error reading user from storage:', error);
+    }
+  };
 
+  const handleLogout = () => {
+    
+    AsyncStorage.removeItem('user');
+    setUser(null);
+  };
 
-
+  const handleLogin = () => {
+    if (loginInput) {
+      axios
+        .get(`https://shufl-be.onrender.com/api/users?username=${loginInput}`)
+        .then((res) => {
+          if (res.data.users.length) {
+            
+            AsyncStorage.setItem('user', JSON.stringify(res.data.users[0]));
+            setUser(res.data.users[0]);
+            setLoginInput('');
+          } else {
+            setLoginError(true);
+            setTimeout(() => setLoginError(false), 3000);
+          }
+        })
+        .catch((error) => {
+          console.error('Error logging in:', error);
+        });
+    }
+  };
 
   return (
     <>
       <NavigationContainer theme={MyTheme}>
         <Pressable style={styles.history} id='history' onPress={() => setHistoryShowing((h) => !h)}>
-          {historyShowing ? 
-          <Text>Hide History</Text> : 
-          <Text>Show History</Text>}
-          {historyShowing ? (<History songHistory={songHistory} />) : <Text></Text>}
+          {historyShowing ? <Text>Hide History</Text> : <Text>Show History</Text>}
+          {historyShowing ? <History songHistory={songHistory} /> : <Text></Text>}
         </Pressable>
-      <AudioPlayer setSongHistory={setSongHistory} user={user}/>
-      <View>
-        {user ? <Text>Welcome {user.username}</Text> : (<><TextInput returnKeyType="send" style={styles.input} onChange={(e) => setLoginInput(e.target.value)} value={loginInput} />
-          <Button title={'Log in'} onPress={() => {
-            if (loginInput){
-              axios.get(`https://shufl-be.onrender.com/api/users?username=${loginInput}`)
-              .then((res) => {
-                if (res.data.users.length){
-                  setUser(res.data.users[0])
-                  setLoginInput('')
-                } else {
-                  setLoginError(true)
-                  setTimeout(()=>setLoginError(false),3000)
-                }
-              })
-            }
-            
-          }} />
-          {loginError?'That user doesnt exist...':''}
-          </>)}
-      </View>
+        <AudioPlayer setSongHistory={setSongHistory} user={user} />
+        <View>
+          {user ? (
+            <Text>
+              Welcome {user.username}{' '}
+              <Button title='Logout' onPress={handleLogout} color='#841584' />
+            </Text>
+          ) : (
+            <>
+              <TextInput
+                returnKeyType='send'
+                style={styles.input}
+                onChange={(e) => setLoginInput(e.target.value)}
+                value={loginInput}
+              />
+              <Button title={'Log in'} onPress={handleLogin} />
+              {loginError ? 'That user doesnt exist...' : ''}
+            </>
+          )}
+        </View>
       </NavigationContainer>
     </>
   );
@@ -72,7 +108,5 @@ const styles = StyleSheet.create({
   history: {
     color: 'white',
     backgroundColor: '#00AFDA',
-  
-  }
-
-})
+  },
+});
