d653225d2f90f07a739be21775a866bbb1da2fba
Nick Diplos <nickd321@gmail.com>
2023-11-17 05:00:06 -0500

    moved directory and fixed code for script

--- nodeModuleConfig.js
+++ nodeModuleConfig.js
@@ -1,10 +1,18 @@
 const fs = require('fs')
 const path = require('path')
 
-const filePath = path.join(__dirname, 'node_modules', 'expo_av', 'build', 'AV.js')
+const filePath = path.join(__dirname, 'node_modules', 'expo-av', 'build', 'AV.js')
 
 const data = fs.readFileSync(filePath, 'utf-8')
 
 const newData = data.replace(/typeof source === 'string' && Platform.OS === 'web'/g, /typeof source === 'string'/)
 
-fs.writeFile(filePath, newData, 'utf-8', (err) => console.log(err))
+try {
+    fs.writeFileSync(filePath, newData, 'utf-8')
+    
+}
+catch (err) {
+    console.log(err)
+}
+
+console.log("SUCCESS")
